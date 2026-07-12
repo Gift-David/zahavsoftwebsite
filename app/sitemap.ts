944ab9b2e3f3a6862b6fff1f4d61b24540d4f480
David Gift <giftdavid189@gmail.com>
2026-07-12 03:14:25 +0100

    update sitemaps

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,49 +3,49 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://zahavsoft.vercel.app',
+      url: 'https://zahavsoft.com',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 1,
     },
     {
-      url: 'https://zahavsoft.vercel.app/about',
+      url: 'https://zahavsoft.com/about',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://zahavsoft.vercel.app/contact',
+      url: 'https://zahavsoft.com/contact',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://zahavsoft.vercel.app/projects',
+      url: 'https://zahavsoft.com/projects',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://zahavsoft.vercel.app/services/branding',
+      url: 'https://zahavsoft.com/services/branding',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://zahavsoft.vercel.app/services/web-development',
+      url: 'https://zahavsoft.com/services/web-development',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://zahavsoft.vercel.app/services/[slug]',
+      url: 'https://zahavsoft.com/services/[slug]',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://zahavsoft.vercel.app/services/ui-ux-design',
+      url: 'https://zahavsoft.com/services/ui-ux-design',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
